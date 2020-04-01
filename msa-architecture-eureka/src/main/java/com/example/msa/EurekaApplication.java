df3affc04588049676ebc39ed4b7497f067b3fcf
foodforme63 <foodforme63@naver.com>
2020-04-02 03:20:42 +0900

    Eureka Server

--- msa-architecture-eureka/src/main/java/com/example/msa/EurekaApplication.java
+++ msa-architecture-eureka/src/main/java/com/example/msa/EurekaApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
+@EnableEurekaServer //Eureka 서버 구성을 위해 필요
 @SpringBootApplication
 public class EurekaApplication {
 
