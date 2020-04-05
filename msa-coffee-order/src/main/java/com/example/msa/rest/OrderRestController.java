95234daacf8447c2a06a01089f27b933ff3ab487
KSH94 <kimseok1116@naver.com>
2020-04-05 20:22:37 +0900

    @EnableCircuitBreaker @HystrixCommand

--- msa-coffee-order/src/main/java/com/example/msa/rest/OrderRestController.java
+++ msa-coffee-order/src/main/java/com/example/msa/rest/OrderRestController.java
@@ -3,6 +3,7 @@ package com.example.msa.rest;
 import com.example.msa.rest.dto.OrderResponseDto;
 import com.example.msa.rest.dto.OrderSaveRequestDto;
 import com.example.msa.service.OrderService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -15,18 +16,21 @@ public class OrderRestController {
 
     private final OrderService orderService;
 
+    @HystrixCommand
     @ApiOperation(value = "save order")
     @PostMapping("/api/v1/orders")
     public Integer save(@RequestBody OrderSaveRequestDto requestDto) {
         return orderService.save(requestDto);
     }
 
+    @HystrixCommand
     @ApiOperation(value = "find all order")
     @GetMapping("/api/v1/orders/")
     public List<OrderResponseDto> findAll() {
         return orderService.findAll();
     }
 
+    @HystrixCommand
     @ApiOperation(value = "find order by id")
     @GetMapping("/api/v1/orders/{id}")
     public OrderResponseDto findById(@PathVariable("id") Long id) {
